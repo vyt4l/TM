e0a4fc1285c8c993ee4da63f668295c74ea0817d
Vital <thomasleguenpro@proton.me>
2026-01-07 17:59:16 +0100

    TM2 , création de decomposePhrase() , bugs a fix

--- s2/TM1/EXO2.java
+++ s2/TM1/EXO2.java
@@ -34,8 +34,41 @@ class EXO2 {
     }
     return res;
   }
+  
+  static String[] decomposePhrase(String s){  
+    int wordn = 0;
+    for(int z = 0 ; z < s.length() ; z++){
+      if(s.charAt(z) == ' ' || z == s.length() - 1){
+        wordn++;
+        while(s.charAt(z) == ' '){
+          z++;
+        }
+      }
+    }
+    String[] res = new String[wordn]; 
+    String tmp = "";
+    int x = 0;
+    for(int i = 0 ; i < s.length() ; i++){
+      while(s.charAt(i) != ' ' || i != s.length()){
+        tmp += s.charAt(i);
+        i++;
+      }
+      if(s.charAt(i - 1) != ' '){
+        res[x] = tmp;
+        x++;
+        tmp = "";
+      }
+    }
+    return res;
+  }
 
   public static void main(String[] args) {
     IO.println(remplacelettre("zaazaa" , 'a' , 'i'));
+    String[] decphrs = decomposePhrase("gougou gaga  hihi   haha");
+    IO.print("[");
+    for(int i = 0 ; i < decphrs.length ; i++){
+      IO.print(decphrs[i] + ",");
+    }
+    IO.println("]");
   }
 }
