0c3ea46f9040d42defb991f66a80ad70d6914885
Vital <thomasleguenpro@proton.me>
2026-01-14 15:34:22 +0100

    Question 5

--- s2/vital/TM2/MyTableau.java
+++ s2/vital/TM2/MyTableau.java
@@ -10,13 +10,61 @@ class MyTableau
   }
 
   public void add(int val)
-  {
-    int[] res = new int[this.tabint.length + 1];
-    for(int i = 9 ; i < res.length - 1 ; i++)
+  { 
+    if(int1 != 0)
     {
-      res[i] = tabing[i];
+    int1 ++;
     }
-    res[res.length - 1] = val;
+    if(int1 == tabint.length)
+    {
+      int[] res = new int[tabint.length + 1];
+      for(int  i = 0 ; i < res.length - 1 ; i++)
+      {
+        res[i] = tabint[i];
+      }
+      res[tabint.length] = val;
+      tabint = res;
+    }
+    else
+    {
+      tabint[int1] = val;
+    }
+  }
+
+  public void delete(int pos)
+  {
+    int1 --;
+    for(int i = pos ; i < tabint.length - 1 ; i++)
+    {
+      tabint[i] = tabint[i + 1];
+    }
+  }
+
+  public int get(int pos)
+  {
+    return tabint[pos];
+  }
+
+  public boolean estTrie()
+  {
+    for(int i = 0 ; i < int1 - 1 ; i++)
+    {
+      if(tabint[i] > tabint[i + 1])
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args)
+  {
+    MyTableau tab1 = new MyTableau(3);
+    tab1.add(1);
+    tab1.add(2);
+    tab1.add(3);
+    tab1.add(4);
+    IO.println(tab1.tabint[3]);
   }
 
 }
