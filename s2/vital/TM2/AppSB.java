d0feda13c91ef1d1d0ffd35a33c7b51764cf74c8
Vital <thomasleguenpro@proton.me>
2026-01-14 14:24:12 +0100

    Question 4

---
+++ s2/vital/TM2/AppSB.java
@@ -0,0 +1,13 @@
+class AppSB
+{
+  public static void main(String[] args)
+  {
+    String c = "ca";
+    String tmp = "";
+    StringBuilder sb = new StringBuilder(tmp);
+    for(int i = 0 ; i < 100000 ; i++)
+    {
+      sb.append(c);
+    }
+  }
+}
---
+++ s2/vital/TM2/AppSBv1.java
@@ -0,0 +1,12 @@
+class AppSBv1
+{
+  public static void main(String[] args)
+  {
+    String c = "ca";
+    String tmp = "";
+    for(int i = 0 ; i < 10000 ; i++)
+    {
+      tmp += c;
+    }
+  }
+}
