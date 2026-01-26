3d8f26f81aee22ad3435c48ec437c34702b8f441
Vital <thomasleguenpro@proton.me>
2026-01-26 10:13:24 +0100

    Question 4 de manière plus mathématiquement acceptable

--- s2/vital/TM3/Question4.java
+++ s2/vital/TM3/Question4.java
@@ -5,17 +5,20 @@ class Question4
     Rectangle box = new Rectangle(0 , 0 , 500 , 500);
     box.setColor(Color.WHITE);
     box.draw();
-    Line segment1 = new Line(100, 0, 200, 0);
-    Line segment2 = new Line(200, 0, 250, 86.6);
-    Line segment3 = new Line(250, 86.6, 200, 173.2);
-    Line segment4 = new Line(200, 173.2, 100, 173.2);
-    Line segment5 = new Line(100, 173.2, 50, 86.6);
-    Line segment6 = new Line(50, 86.6, 100, 0);
-    segment1.draw();
-    segment2.draw();
-    segment3.draw();
-    segment4.draw();
-    segment5.draw();
-    segment6.draw();
+    int[] x = new int[6];
+    int[] y = new int[6];
+    double angle = Math.toRadians(360/6);
+    for(int i = 0 ; i < 6 ; i++)
+    {
+      x[i] = (int) (100 * Math.cos(i* angle));
+      y[i] = (int) (100 * Math.sin(i* angle));
+    }
+    
+    for(int z = 0 ; z < 6 ;z++)
+    {
+      int nextIndex = (z + 1) % 6;
+      Line seg = new Line(x[z] +300  , y[z] +300 , x[nextIndex] +300 , y[nextIndex] +300);
+      seg.draw();
+    }
   }
 }
--- s2/vital/TM3/Question6.java
+++ s2/vital/TM3/Question6.java
@@ -3,38 +3,38 @@ import javax.swing.*;
 
 public class Question6 
 {
-    public static void main(String[] args) 
-    {
-        JFrame frame = new JFrame();
-        frame.setSize(300, 300); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Hexagone hexa = new Hexagone();
-        frame.add(hexa);
-        frame.setVisible(true);        
-        JFrame frame2 = new JFrame();
-        frame2.setSize(30*8, 30*8); 
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Echecs ech = new Echecs(Integer.valueOf(IO.readln("Coo x ? : ")) , Integer.valueOf(IO.readln("Coo y ? : ")) );
-        frame2.add(ech);
-        frame2.setVisible(true);
-    }
+  public static void main(String[] args) 
+  {
+    JFrame frame = new JFrame();
+    frame.setSize(300, 200); 
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Hexagone hexa = new Hexagone();
+    frame.add(hexa);
+    frame.setVisible(true);        
+    JFrame frame2 = new JFrame();
+    frame2.setSize(30*8, 30*8); 
+    frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Echecs ech = new Echecs(Integer.valueOf(IO.readln("Coo x ? : ")) , Integer.valueOf(IO.readln("Coo y ? : ")) );
+    frame2.add(ech);
+    frame2.setVisible(true);
+  }
 }
 
 class Hexagone extends JComponent 
 {
-    public void paintComponent(Graphics g) 
-    {
-        Graphics2D g2 = (Graphics2D) g;
+  public void paintComponent(Graphics g) 
+  {
+    Graphics2D g2 = (Graphics2D) g;
 
-        g2.setStroke(new BasicStroke(1));
-        g2.setColor(Color.GREEN);
-        g2.drawLine(100, 0, 200, 0);        
-        g2.drawLine(200, 0, 250, 86);     
-        g2.drawLine(250, 86, 200, 173); 
-        g2.drawLine(200, 173, 100, 173);
-        g2.drawLine(100, 173, 50, 86);  
-        g2.drawLine(50, 86, 100, 0);      
-    }
+    g2.setStroke(new BasicStroke(5));
+    g2.setColor(Color.BLUE);
+    g2.drawLine(100, 0, 200, 0);        
+    g2.drawLine(200, 0, 250, 86);     
+    g2.drawLine(250, 86, 200, 173); 
+    g2.drawLine(200, 173, 100, 173);
+    g2.drawLine(100, 173, 50, 86);  
+    g2.drawLine(50, 86, 100, 0);      
+  }
 }
 
 class Echecs extends JComponent 
