f714f52c3ec0ea70fa60a0e4526080323d02c051
Vital <thomasleguenpro@proton.me>
2026-02-09 10:04:01 +0100

    un peu plus de lisibilité

--- s2/vital/TM4/EXO4.java
+++ s2/vital/TM4/EXO4.java
@@ -1,19 +1,15 @@
 class EXO4{
   
   public static boolean palindrome(String s, int i , int j){
-    if(i == j - 1 || i == j) {return s.charAt(i) == s.charAt(j - 1);}
-    else if(s.charAt(j-1) == s.charAt(i)){return palindrome(s , i+1 , j-1);}
-    else{return false;}
+    if (i == j - 1 || i == j) return s.charAt(i) == s.charAt(j - 1);
+    else if (s.charAt(j-1) == s.charAt(i)) return palindrome(s , i+1 , j-1);
+    else return false;
   }
   public static boolean palindrome(String s){ return palindrome(s , 0 , s.length());}
 
   public static String verlan(String s , int i , String res){
-    if (i == 0){
-      return res + s.charAt(i);
-    }
-    else {
-      return verlan(s , i-1 , res + s.charAt(i));
-    }
+    if (i == 0) return res + s.charAt(i);
+    else return verlan(s , i-1 , res + s.charAt(i));
   }
   public static String verlan(String s){return verlan(s , s.length() - 1 , "");} 
 
