f5d3069d33e4ec5b9ab511fff0097bd7a8614f70
Vital <thomasleguenpro@proton.me>
2026-02-16 10:07:13 +0100

    corrigé des exos pour les faire de la bonne manière

--- s2/vital/TM4/EXO4.java
+++ s2/vital/TM4/EXO4.java
@@ -1,17 +1,15 @@
 class EXO4{
   
-  public static boolean palindrome(String s, int i , int j){
-    if (i == j - 1 || i == j) return s.charAt(i) == s.charAt(j - 1);
-    else if (s.charAt(j-1) == s.charAt(i)) return palindrome(s , i+1 , j-1);
+  public static boolean palindrome(String s){
+    if (s.length() == 2 || s.length() == 1) return s.charAt(0) == s.charAt(s.length()-1);
+    else if (s.charAt(0) == s.charAt(s.length() - 1)) return palindrome(s.substring(1 , s.length() - 1));
     else return false;
   }
-  public static boolean palindrome(String s){ return palindrome(s , 0 , s.length());}
 
-  public static String verlan(String s , int i , String res){
-    if (i == 0) return res + s.charAt(i);
-    else return verlan(s , i-1 , res + s.charAt(i));
+  public static String verlan(String s){
+    if (s.length() <= 1) return s;
+    return verlan(s.substring(1)) + s.charAt(0);
   }
-  public static String verlan(String s){return verlan(s , s.length() - 1 , "");} 
 
   public static void main(String[] args){
     IO.println(palindrome(args[0]));
