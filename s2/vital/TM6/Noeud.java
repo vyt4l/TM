616c90629c415762d34aa92f55c3ea5fb49fe62e
Vital <thomasleguenpro@proton.me>
2026-02-20 22:43:33 +0100

    EXO 3, fin du TM

--- s2/vital/TM6/Noeud.java
+++ s2/vital/TM6/Noeud.java
@@ -14,7 +14,33 @@ public class Noeud {
             n.droit = creation((int)(Math.random()*500.0), prof + 1);
     }
     return n;
-}
+    }
+  
+  public int sommearbr(){
+    int sum = 0;
+    sum += this.valeur;
+    if(this.droit != null){
+      sum += this.droit.sommearbr();
+    }
+    if(this.gauche != null){
+      sum += this.gauche.sommearbr();
+    }
+    return sum;
+  }
+  public boolean bfs(int x){
+    if (this.valeur == x) return true;
+    if(this.droit != null && this.droit.bfs(x)){
+      return true;
+    }
+    if(this.gauche != null && this.gauche.bfs(x)){
+      return true;
+    }
+    return false;
+  }
 
-
+  public static void main(String[] args){
+    Noeud n = creation(12,5);
+    IO.println(n.sommearbr());
+    IO.println(n.bfs(11));
+  }
 }
